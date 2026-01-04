7c9f82e84176ca1287242150c77c44b0fa0fe35e
lb9kj <lb9kj@localhost>
2026-01-04 14:48:35 +0100

    Updated the Assembly information

--- StationMonitor/Properties/AssemblyInfo.cs
+++ StationMonitor/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StationMonitor")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
